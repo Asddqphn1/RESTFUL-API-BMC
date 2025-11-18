e3839fee793f13531d66ce86f5fda6f5fbe325e4
Asddqphn1 <assiddiqiphn@gmail.com>
2025-11-18 16:02:43 +0700

    fix: Enhance index.php for Vercel compatibility with storage path adjustments

--- public/index.php
+++ public/index.php
@@ -1,20 +1,54 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+*/
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
+
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// --- TAMBAHAN KHUSUS VERCEL (Paste di sini) ---
+$app->useStoragePath('/tmp/storage');
+
+if (!is_dir('/tmp/storage')) {
+    mkdir('/tmp/storage');
+    mkdir('/tmp/storage/app');
+    mkdir('/tmp/storage/framework');
+    mkdir('/tmp/storage/framework/cache');
+    mkdir('/tmp/storage/framework/sessions');
+    mkdir('/tmp/storage/framework/views');
+    mkdir('/tmp/storage/logs');
+}
+// ---------------------------------------------
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
